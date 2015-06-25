9cae5765c069042c4ae1c305fc1c9af1f6aa3d3a
hbomb79 <harryfelton12@gmail.com>
2015-06-25 17:04:58 +1200

    Fixed wrong name

--- assets/_module/imports.php
+++ assets/_module/imports.php
@@ -8,5 +8,5 @@
 <script src="assets/javascript/scroller.js"></script>
 <script src="assets/javascript/hash.js"></script>
 <script src="assets/javascript/hexslide.min.js"></script>
-<link rel="stylesheet" href="assets/css/slide.css">
+<link rel="stylesheet" href="assets/css/hexslide.css">
 <!-- END HEAD IMPORTS -->
